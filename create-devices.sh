b34e98b33e2cbf364dac04dcbe224c0385eb30e0
Samruddhi Khandale <samruddhikhandale@github.com>
2023-03-17 21:10:38 +0000

    devices are automatically mapped

---
+++ create-devices.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -ex
+
+sudo dmsetup remove_all
+sudo losetup -D
+
+DATA_DIR=/var/lib/containerd/devmapper
+POOL_NAME=devpool
+
+sudo mkdir -p ${DATA_DIR}
+
+# Create data file
+sudo touch "${DATA_DIR}/data"
+# sudo truncate -s 100G "${DATA_DIR}/data"
+sudo truncate -s 100G "${DATA_DIR}/data" > /dev/null 2>&1
+
+# Create metadata file
+sudo touch "${DATA_DIR}/meta"
+sudo truncate -s 10G "${DATA_DIR}/meta"
+
+# Allocate loop devices
+DATA_DEV=$(sudo losetup --find --show "${DATA_DIR}/data")
+META_DEV=$(sudo losetup --find --show "${DATA_DIR}/meta")
+
+# Define thin-pool parameters.
+# See https://www.kernel.org/doc/Documentation/device-mapper/thin-provisioning.txt for details.
+SECTOR_SIZE=512
+DATA_SIZE="$(sudo blockdev --getsize64 -q ${DATA_DEV})"
+LENGTH_IN_SECTORS=$(bc <<< "${DATA_SIZE}/${SECTOR_SIZE}")
+DATA_BLOCK_SIZE=128
+LOW_WATER_MARK=32768
+
+# Create a thin-pool device
+sudo dmsetup create "${POOL_NAME}" \
+    --table "0 ${LENGTH_IN_SECTORS} thin-pool ${META_DEV} ${DATA_DEV} ${DATA_BLOCK_SIZE} ${LOW_WATER_MARK}"
+
+sudo dmsetup info
+sudo ls /dev/mapper
